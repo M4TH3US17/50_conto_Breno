8125b48f982124b52d9a1b43c802ecd86cf65568
BrenoRev <silvabreno462@gmail.com>
2023-02-08 23:59:27 -0300

    feat/refact-implements

--- src/main/java/com/ubivis/controller/MachineHaltController.java
+++ src/main/java/com/ubivis/controller/MachineHaltController.java
@@ -1,16 +1,17 @@
 package com.ubivis.controller;
 
-import com.ubivis.entity.Machine_halt;
+import com.ubivis.entity.MachineHalt;
+import com.ubivis.entity.dto.MachineHaltDTO;
 import com.ubivis.service.MachineHaltService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.time.LocalTime;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @RestController
@@ -25,20 +26,24 @@ public class MachineHaltController {
     private ModelMapper modelMapper;
 
     @PostMapping(value = "/machine-halt", produces = "application/json", consumes = "application/json")
-    public ResponseEntity<Machine_halt> salvar(@RequestBody Machine_halt machine_halt){
-        Machine_halt object = service.salvar(machine_halt);
+    public ResponseEntity<MachineHalt> salvar(@RequestBody MachineHalt machine_halt){
+        MachineHalt object = service.salvar(machine_halt);
         return ResponseEntity.status(HttpStatus.CREATED).body(object);
     }
 
     @GetMapping(value = "/machine-halt/list", produces = "application/json")
-    public ResponseEntity<List<Machine_halt>> listar(@RequestParam(required = false) LocalTime interval_start,
-                                                     @RequestParam(required = false) LocalTime interval_end,
-                                                     @RequestParam(required = false) String machine_tag){
-        return ResponseEntity.ok().body(service.listaMachineHalt(machine_tag, interval_start, interval_end));
+    public ResponseEntity<List<MachineHalt>> listar(@RequestParam("interval_start")
+                                                         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                            ZonedDateTime intervalStart,
+                                                     @RequestParam("interval_end")
+                                                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                            ZonedDateTime intervalEnd,
+                                                     @RequestParam("machine_tag") String machine_tag) {
+        return ResponseEntity.ok().body(service.listaMachineHalt(machine_tag, intervalStart.toLocalDateTime(), intervalEnd.toLocalDateTime()));
     }
 
     @GetMapping(value = "/machine-halt/{id}", produces = "application/json")
-    public ResponseEntity<Machine_halt> buscarPorId(@PathVariable("id") Long id) throws Exception {
+    public ResponseEntity<MachineHalt> buscarPorId(@PathVariable("id") Long id) throws Exception {
         return ResponseEntity.ok().body(service.buscarPorId(id));
     }
 
@@ -48,8 +53,8 @@ public class MachineHaltController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping(value = "/machine-halt/{id}", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Machine_halt> atualizar(@PathVariable("id") Long idMachine, @RequestBody Machine_halt machine) throws Exception {
-        return ResponseEntity.status(HttpStatus.OK).body(service.atualizar(idMachine, machine));
+    @PutMapping(value = "/machine-halt", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<MachineHalt> atualizar(@RequestBody MachineHaltDTO machine) throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(service.atualizar(machine));
     }
 }
